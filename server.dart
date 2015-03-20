a33bf03ee1712d6c8511afaab42ca498b8f24a37
Jermaine Oppong <jeropp00@gmail.com>
2015-03-20 22:51:14 +0000

    Refactor server.dart to resolve MIME type issue with loading JavaScript files.

--- server.dart
+++ server.dart
@@ -4,16 +4,37 @@ import "package:http_server/http_server.dart" show VirtualDirectory;
 final HOST = InternetAddress.LOOPBACK_IP_V4;
 final PORT = 4567;
 
+VirtualDirectory virDir;
+
+void directoryHandler(directory, request) {
+  var indexUri = new Uri.file(directory.path).resolve('index.html');
+  virDir.serveFile(new File(indexUri.toFilePath()), request);
+}
+
+void errorPageHandler(HttpRequest request) {
+  request.response
+    ..statusCode = HttpStatus.NOT_FOUND
+    ..write('Not found')
+    ..close();
+}
+
 void handleError(error) {
-  print("Problems: $error");  
+  print("Problems: $error");
 }
 
 void main() {
-  VirtualDirectory virDir = new VirtualDirectory('web');
+  var buildPath = 'web';
+
+  virDir = new VirtualDirectory(buildPath)
+    ..allowDirectoryListing = true
+    ..directoryHandler = directoryHandler
+    ..errorPageHandler = errorPageHandler;
 
   HttpServer.bind(HOST, PORT).then((server) {
     server.listen((request) {
-      virDir.serveFile(new File('web/index.html'), request);
+      print(request.uri);
+
+      virDir.serveRequest(request);
     }).onError(handleError);
   }).catchError(handleError);
 }
