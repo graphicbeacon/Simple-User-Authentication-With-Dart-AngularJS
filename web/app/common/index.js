146e5910b1be2f0c6f1206aa1a3fa7ec64c848ed
Jermaine Oppong <jeropp00@gmail.com>
2015-03-21 21:55:12 +0000

    Add httpProvider interceptors for middleware routing.

--- web/app/common/index.js
+++ web/app/common/index.js
@@ -7,7 +7,7 @@
 
 
 
-    function RoutesConfig (Route) {
+    function RoutesConfig (Route, HttpProvider) {
 
         Route.when('/', {
             templateUrl: '/app/account/index.html' ,
@@ -23,14 +23,48 @@
             redirectTo: '/'
         });
 
+        HttpProvider.interceptors.push('RouteInterceptors');
     };
 
     RoutesConfig.$inject = ['$routeProvider', '$httpProvider'];
 
 
 
+
+    function RouteInterceptors(Q, Location) {
+
+        var request = function(config) {
+
+            if('undefined' == typeof config.headers['SimpleAppAuthToken']) {
+                Location.path('/login');
+            }
+
+            return config;
+        };
+
+        var requestError = function(error) {
+            return Q.reject(error);
+        };
+
+        var response = function(response) {
+            return response;
+        };
+
+        return {
+            request: request,
+            requestError: requestError,
+            response: response
+        };
+    }
+
+    RouteInterceptors.$inject = ['$q', '$location'];
+
+
+
+    // Module Setup
     angular.module('SimpleApp', ['ngRoute'])
         .controller('SimpleAppController', SimpleAppController)
+        .factory('RouteInterceptors', RouteInterceptors)
         .config(RoutesConfig);
 
 })();
